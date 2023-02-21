c4fdb4ddb267ad24cdf8baebbabed9e13536faea
44004098 <121195860+44004098@users.noreply.github.com>
2023-02-21 15:13:20 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,6 +1,19 @@
-# Name:
+# Name: Kiana 
 
 class Card:
+    def __init__(self,value,suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        return 'value={self.value},suit={self.suit}'
+    def __str__(self):
+        return f"{self.value} of {self.suit}"
+    def __eq__(self, other):
+        if Card((self.value) = (self.other))
+        return True 
+
+
     """class representing a simple Neapolitan card. Holds a 
     value and a suit"""
     
